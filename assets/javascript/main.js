fb0a43a604d6f6cae88ad19ad338da0f61c76c68
jamesdoh0109 <magicjcdp@gmail.com>
2021-05-27 05:37:33 +0900

    first upload for fibonacci website

---
+++ assets/javascript/main.js
@@ -0,0 +1,30 @@
+function fibonacci(num) {
+    if (isNaN(num) || num < 1 || num % 1 != 0) {
+        return "Please enter a valid number (a positive integer greater than 0)."
+    }
+
+    let str = "";
+    let prev = 0;
+    let curr = 1; 
+    let next = 1; 
+    for (let i = 0; i < num; i++) {
+        if (i != num - 1) {
+            str += prev + ", ";
+            prev = curr; 
+            curr = next; 
+            next = prev + curr; 
+        } else {
+            str += prev
+        }
+    }
+    return str; 
+}
+
+var submit = document.getElementById("button");
+submit.addEventListener("click", function() {
+    var number = document.getElementById("number").value;
+    document.getElementById("fibscreen").innerHTML = fibonacci(number);
+});
+
+
+
